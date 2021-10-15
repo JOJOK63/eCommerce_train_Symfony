c650ec0f707b1a7f42f7c445121aacd5500eb92c
JOJOK63 <krariajordan@gmail.com>
2021-10-15 10:12:02 +0200

    modification bootstrap and add propertie to user

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +13,14 @@ class RegisterController extends AbstractController
     #[Route('/inscription', 'register')]
     public function index(): Response
     {
-        return $this->render('register/index.html.twig');
+        //instanciation of the class User ,because we gonna have a new user
+        $user = new User();
+        //instanciation du formulaire , on lui injecte la Class register type et les data de user
+        $form = $this->createForm(RegisterFormType::class,$user);
+
+        //we get the view and set a table with variable form (and this variable create the view form
+        return $this->render('register/index.html.twig', [
+            'form'=> $form->createView()
+        ]);
     }
 }
