290fa065370b107954897a1fa75710b36e1c0182
JOJOK63 <krariajordan@gmail.com>
2021-10-15 13:32:02 +0200

    implementation password encoder but is deprecated , need to change ??

--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -3,7 +3,13 @@
 namespace App\Form;
 
 use App\Entity\User;
+use PharIo\Manifest\Email;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +18,39 @@ class RegisterFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+//                to put a placeholder ,
+                'attr' => [
+                    'placeholder' => 'Saisir votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre Email',
+                'attr' => [
+                    'placeholder' => "Saisir votre mot de passe"
+                ]
+            ])
+            ->add('password',RepeatedType::class,[
+                'invalid_message' => 'le mot de passe et la confirmation doivent etre identique',
+                'label' => 'Votre mot de passe',
+                'required' => true,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmez Mot de passe']
+            ])
+
+            ->add('submit',SubmitType::class,[
+                'label' => 'S\'Inscrire'
+            ])
+
         ;
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
