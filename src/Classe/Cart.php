f33dd0f53b3fb74be9b2b2a4d2b615a39160d4a5
JOJOK63 <krariajordan@gmail.com>
2021-10-19 22:45:59 +0200

    reorganisation cart , cartcontroller and view , add , delete product of the cart

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,15 +2,21 @@
 
 namespace App\Classe;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart
 {
     private $session;
+    private $entityManager;
 
-    public function __construct(SessionInterface $session)
+    public function __construct(SessionInterface $session,EntityManagerInterface $entityManager , ProductRepository $productRepository)
     {
         $this->session = $session;
+        $this->entityManager = $entityManager;
+
     }
 
     public function add($id)
@@ -34,5 +40,48 @@ class Cart
     public function remove()
     {
         return $this->session->remove('cart');
+    }
+
+    public function delete($id){
+
+        $cart = $this->session->get('cart',[]);
+
+        //keep out cart with the id select out of the cart array
+        unset($cart[$id]);
+
+        return $this->session->set('cart',$cart);
+    }
+
+    public function decrease($id)
+    {
+        $cart = $this->session->get('cart',[]);
+
+        if($cart[$id] > 1) {
+            $cart[$id]--;
+        }else{
+            unset($cart[$id]);
+        }
+        return $this->session->set('cart',$cart);
+    }
+
+    public function getFull() {
+
+        $cartComplete = [];
+
+        if($this->get()){
+            foreach ($this->get() as  $id => $quantity) {
+                $product_object= $this->entityManager->getRepository(Product::class)->findOneById($id);
+//                security: if someone put an inexistant id the app make a verification and delete the user id entrance
+                if(!$product_object){
+                    $this->delete($id);
+                    continue;
+                }
+                $cartComplete[] = [
+                    'product' => $product_object,
+                    'quantity' => $quantity
+                ];
+            }
+        }
+        return $cartComplete;
     }
 }
